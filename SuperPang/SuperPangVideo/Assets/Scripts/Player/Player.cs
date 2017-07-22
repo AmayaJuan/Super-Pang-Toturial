3386d523841b86086d47d0e2b105f3caaf8ef393
AmayaJuan <jpva360@gmail.com>
2017-07-22 14:32:48 -0500

    tutorial 3

---
+++ SuperPang/SuperPangVideo/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 4f;
+    float movement = 0;
+    Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer sr;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        movement = Input.GetAxisRaw("Horizontal") * speed;
+        animator.SetInteger("VelX", Mathf.RoundToInt(movement));
+        if (movement < 0)
+            sr.flipX = true;
+        else
+            sr.flipX = false;
+	}
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Vector2.right * movement * Time.fixedDeltaTime);
+    }
+}
