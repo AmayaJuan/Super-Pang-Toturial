9ecf11ac590bb378420190d41d81a52af9d9f65f
AmayaJuan <jpva360@gmail.com>
2017-07-24 11:23:54 -0500

    Tutorial 6
    Disparos - Flecha 2

--- SuperPang/SuperPangVideo/Assets/Scripts/Shots/ShotArrow.cs
+++ SuperPang/SuperPangVideo/Assets/Scripts/Shots/ShotArrow.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class ShotArrow : MonoBehaviour
 {
+    public GameObject chainGFX;
+
     float speed = 4f;
+    Vector2 startPos;
 
 	void Start ()
     {
-		
-	}
+        startPos = transform.position;
+        InstantiateChainGFX();
+    }
 	
 	void Update ()
     {
         transform.position += Vector3.up * speed * Time.deltaTime;
+
+        if ((transform.position.y - startPos.y) >= .2f)
+            InstantiateChainGFX();
 	}
+
+    void InstantiateChainGFX()
+    {
+        GameObject chain = Instantiate(chainGFX, transform.position, Quaternion.identity);
+        chain.transform.parent = transform;
+        startPos = transform.position;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
