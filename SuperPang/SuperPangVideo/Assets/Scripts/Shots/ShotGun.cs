2c22f63cd983e0396f5b94d630638a03a51ec40a
AmayaJuan <jpva360@gmail.com>
2017-09-07 14:24:44 -0500

    Tutorial 7
    7 - Creando Videojuegos Desde 0 Con Unity 5 - Super Pang (Disparos 3 - Ancla & Laser )

---
+++ SuperPang/SuperPangVideo/Assets/Scripts/Shots/ShotGun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ShotGun : MonoBehaviour
+{
+    float speed = 8;
+
+    void Update()
+    {
+        if (transform.rotation.z == 0)
+            transform.position += Vector3.up * Time.deltaTime * speed;
+        else if (transform.rotation.z < 0)
+            transform.position += new Vector3(.1f, 1) * Time.deltaTime * speed;
+        else   
+            transform.position += new Vector3(-.1f, 1) * Time.deltaTime * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
