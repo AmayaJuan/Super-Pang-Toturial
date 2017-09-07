9408fa9e13e0503ce6f1c8d67542ca25930fbd5b
AmayaJuan <jpva360@gmail.com>
2017-09-06 22:52:39 -0500

    Tutorial 3
    3 - Creando Videojuegos Desde 0 Con Unity 5 - Super Pang (Creando al Player)

---
+++ SuperPang/SuperPangVideo/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float movement = 0;
+    float speed = 4f;
+    Animator animator;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        movement = Input.GetAxisRaw("Horizontal") * speed;
+        animator.SetInteger("VelX", Mathf.RoundToInt(movement));
+
+        if (movement < 0)
+            sr.flipX = true;
+        else
+            sr.flipX = false;
+	}
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Vector2.right * movement * Time.fixedDeltaTime);
+    }
+}
