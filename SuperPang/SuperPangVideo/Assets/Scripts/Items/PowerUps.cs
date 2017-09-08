d091aed674fb427d6a1e34ff8906f7b947f76d18
AmayaJuan <jpva360@gmail.com>
2017-09-07 23:49:53 -0500

    Tutorial 10
    10 - Creando Videojuegos Desde 0 Con Unity 5 - Super Pang (Cambio de disparo 1)

---
+++ SuperPang/SuperPangVideo/Assets/Scripts/Items/PowerUps.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    public GameObject[] powerUpsAnimated;
+    public Sprite[] powerUpsStatic;
+
+    bool inGround;
+    SpriteRenderer sr;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Start ()
+    {
+        int aleatory = Random.Range(0, 2);
+
+        if (aleatory == 0)
+        {
+            sr.sprite = powerUpsStatic[Random.Range(0, powerUpsStatic.Length)];
+            gameObject.name = sr.sprite.name;
+        }
+	}
+	
+	void Update ()
+    {
+        if (!inGround)
+            transform.position += Vector3.down * Time.deltaTime * 2;
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            inGround = true;
+            Destroy(gameObject, 15);
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            if (gameObject.name.Equals("DoubleArrow"))
+                ShotManager.shm.ChangeShot(1);
+            else if (gameObject.name.Equals("Ancle"))
+                ShotManager.shm.ChangeShot(2);
+            else if (gameObject.name.Equals("Gun"))
+                ShotManager.shm.ChangeShot(3);
+
+            Destroy(gameObject);
+        }     
+    }
+}
