70166afb449e5187c5d8fd40a808d137ede8432f
AmayaJuan <jpva360@gmail.com>
2017-09-14 14:09:34 -0500

    Player
    Script and animation

---
+++ SuperPang/SuperPangVideo/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float movementX = 0;
+    float speedX = 4.0f;
+    Animator animator;
+    SpriteRenderer sr;
+    Rigidbody2D rb;
+    
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        movementX = Input.GetAxisRaw("Horizontal") * speedX;
+        animator.SetInteger("VelX", Mathf.RoundToInt(movementX));
+
+        if (movementX < 0)
+            sr.flipX = true;
+        else
+            sr.flipX = false;
+	}
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Vector2.right * movementX * Time.fixedDeltaTime);
+    }
+}
