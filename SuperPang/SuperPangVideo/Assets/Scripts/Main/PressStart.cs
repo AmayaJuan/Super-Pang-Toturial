fae919e5d654a8d654fcd717dd3e4f512b3812c5
AmayaJuan <jpva360@gmail.com>
2017-09-14 12:22:55 -0500

    Tutorial 1, 2
    Selemode y Main

---
+++ SuperPang/SuperPangVideo/Assets/Scripts/Main/PressStart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressStart : MonoBehaviour
+{
+    public GameObject pressStart;
+
+    float time;
+	
+	void Update ()
+    {
+        time += Time.deltaTime;
+
+        if (Mathf.RoundToInt(time) % 2 == 0)
+            pressStart.SetActive(true);
+        else
+            pressStart.SetActive(false);
+
+        if (Input.GetMouseButtonDown(0))
+            SceneManager.LoadScene(1);
+	}
+}
---
+++ SuperPang/SuperPangVideo/Assets/Scripts/Main/SelectMode.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectMode : MonoBehaviour
+{/*
+    public Image tourModeImage;
+    public Image panicModeImage;
+    public Text tourModeText;
+    public Text panicModeText;
+
+    bool tour = true;
+
+    void Update()
+    {
+        if (tour)
+        {
+            tourModeImage.color = new Color(1, 1, 1);
+            tourModeText.color = Color.white;
+
+            panicModeImage.color = new Color(1, 1, 0, .5f);
+            panicModeText.color = new Color(1, 1, 0, .5f);
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+                tour = !tour;
+
+            if (Input.GetKeyDown(KeyCode.Return))
+                SceneManager.LoadScene("Tour_01");
+        }
+        else
+        {
+            panicModeImage.color = new Color(1, 1, 1);
+            panicModeText.color = Color.white;
+
+            tourModeImage.color = new Color(1, 1, 0, .5f);
+            tourModeText.color = new Color(1, 1, 0, .5f);
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                tour = !tour;
+
+            if (Input.GetKeyDown(KeyCode.Return))
+                SceneManager.LoadScene("Panic");
+        }
+    }
+    */
+
+    public void NextNivel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
