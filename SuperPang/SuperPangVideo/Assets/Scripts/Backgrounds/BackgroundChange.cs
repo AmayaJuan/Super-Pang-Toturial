1021e3b2f5b4791b7ecccfb73f2ca0a08017aa39
AmayaJuan <jpva360@gmail.com>
2017-09-28 13:53:46 -0500

    Turorial 25
    25 - Creando Videojuegos Desde 0 Con Unity 5 - Super Pang (Background & Border)

---
+++ SuperPang/SuperPangVideo/Assets/Scripts/Backgrounds/BackgroundChange.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundChange : MonoBehaviour
+{
+    public Sprite[] backgrounds;
+
+    Image currentBackground;
+
+	void Start ()
+    {
+        currentBackground = GetComponent<Image>();
+        currentBackground.sprite = backgrounds[Random.Range(0, backgrounds.Length)];
+	}
+}
