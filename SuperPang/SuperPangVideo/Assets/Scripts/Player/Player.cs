4e2f81738fea1087215fb4d722e167213cdac98b
AmayaJuan <jpva360@gmail.com>
2018-10-11 13:12:31 -0500

    Super Pang
     (Creando al Player)

---
+++ SuperPang/SuperPangVideo/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 4f;
+    float movement = 0;
+    Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer sr;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+	void Update ()
+    {
+        movement = Input.GetAxisRaw("Horizontal") * speed;
+        animator.SetInteger("velX", Mathf.RoundToInt(movement));
+
+        if (movement < 0)
+            sr.flipX = true;
+        else
+            sr.flipX = false;
+	}
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Vector2.right * movement * Time.fixedDeltaTime);
+    }
+}
