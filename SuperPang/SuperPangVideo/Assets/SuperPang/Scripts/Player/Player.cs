a8c816c384324865be9b7abcbede99d2f0bc28fd
AmayaJuan <jpva360@gmail.com>
2018-10-22 13:33:21 -0500

    Super Pang
    (Bolas 1)

--- SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Player/Player.cs
+++ SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Player/Player.cs
@@ -49,7 +49,7 @@ public class Player : MonoBehaviour
         rb.MovePosition(rb.position + Vector2.right * movement * Time.fixedDeltaTime);
     }
 
-    void OnCollisionStay2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Left")
             leftWall = true;
@@ -57,7 +57,7 @@ public class Player : MonoBehaviour
             rightWall = true;
     }
 
-    void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Left")
             leftWall = false;
