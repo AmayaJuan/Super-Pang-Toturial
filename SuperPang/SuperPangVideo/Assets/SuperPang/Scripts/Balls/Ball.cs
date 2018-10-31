23932b4e49046b816dc90a0b8ef6da337019b25f
AmayaJuan <jpva360@gmail.com>
2018-10-31 14:42:35 -0500

    Super Pang
    (Bolas 2)

--- SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Balls/Ball.cs
+++ SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Balls/Ball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public GameObject nextBall;
+    public bool right;
 
     Rigidbody2D rb;
 
@@ -21,9 +22,21 @@ public class Ball : MonoBehaviour
             GameObject ball2 = Instantiate(nextBall, rb.position + Vector2.left / 4, Quaternion.identity);
 
             ball1.GetComponent<Rigidbody2D>().AddForce(new Vector2(2, 5), ForceMode2D.Impulse);
+            ball1.GetComponent<Ball>().right = true;
             ball2.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2, 5), ForceMode2D.Impulse);
+            ball2.GetComponent<Ball>().right = false;
 
             BallManager.bm.DestroyBall(gameObject, ball1, ball2);
         }
+        else
+            BallManager.bm.LastBall(gameObject);
+    }
+
+    public void StartForce(GameObject ball)
+    {
+        if (right)
+            ball.GetComponent<Rigidbody2D>().AddForce(Vector3.right * 2, ForceMode2D.Impulse);
+        else
+            ball.GetComponent<Rigidbody2D>().AddForce(Vector3.left * 2, ForceMode2D.Impulse);
     }
 }
