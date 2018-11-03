c0aeeb5d734a6ab7b06608abbcd22d6b328f3439
AmayaJuan <jpva360@gmail.com>
2018-11-03 16:08:38 -0500

    ffff
    fff

--- SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Items/PowerUps.cs
+++ SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Items/PowerUps.cs
@@ -41,17 +41,12 @@ public class PowerUps : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             if (gameObject.name.Equals("DoubleArrow"))
-            {
                 ShotManager.shm.ChangeShot(1);
-            }
             else if (gameObject.name.Equals("Ancle"))
-            {
                 ShotManager.shm.ChangeShot(2);
-            }
             else if (gameObject.name.Equals("Gun"))
-            {
                 ShotManager.shm.ChangeShot(3);
-            }
+            
             Destroy(gameObject);
         }
     }
