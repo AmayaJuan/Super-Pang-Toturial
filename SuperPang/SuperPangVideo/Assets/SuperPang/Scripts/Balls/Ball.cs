7ec4ef8a6ead9123854fd88d1467c2dbbbf997e3
AmayaJuan <jpva360@gmail.com>
2018-11-09 00:49:37 -0500

    Super Pang
    TimeStop

--- SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Balls/Ball.cs
+++ SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Balls/Ball.cs
@@ -5,6 +5,7 @@ public class Ball : MonoBehaviour
     public GameObject nextBall;
     public bool right;
 
+    Vector2 currentVelocity;
     Rigidbody2D rb;
 
     void Awake()
@@ -19,12 +20,24 @@ public class Ball : MonoBehaviour
             GameObject ball1 = Instantiate(nextBall, rb.position + Vector2.right / 4, Quaternion.identity);
             GameObject ball2 = Instantiate(nextBall, rb.position + Vector2.left / 4, Quaternion.identity);
 
-            ball1.GetComponent<Rigidbody2D>().AddForce(new Vector2(2, 5), ForceMode2D.Impulse);
-            ball1.GetComponent<Ball>().right = true;
-            ball2.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2, 5), ForceMode2D.Impulse);
-            ball2.GetComponent<Ball>().right = false;
+            if (!FreezeManager.fm.freeze)
+            {
+                ball1.GetComponent<Rigidbody2D>().isKinematic = false;
+                ball1.GetComponent<Rigidbody2D>().AddForce(new Vector2(2, 5), ForceMode2D.Impulse);
+                ball1.GetComponent<Ball>().right = true;
 
-            BallManager.bm.DestroyBall(gameObject, ball1, ball2);
+                ball2.GetComponent<Rigidbody2D>().isKinematic = false;
+                ball2.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2, 5), ForceMode2D.Impulse);
+                ball2.GetComponent<Ball>().right = false;
+            }
+            else
+            {
+                ball1.GetComponent<Ball>().currentVelocity = new Vector2(2, 5);
+                ball2.GetComponent<Ball>().currentVelocity = new Vector2(-2, 5);
+            }
+
+            if (!BallManager.bm.spliting)
+                BallManager.bm.DestroyBall(gameObject, ball1, ball2);
         }
         else
             BallManager.bm.LastBall(gameObject);
@@ -32,9 +45,36 @@ public class Ball : MonoBehaviour
 
     public void StartForce(GameObject ball)
     {
+        ball.GetComponent<Rigidbody2D>().isKinematic = false;
+
         if (right)
             ball.GetComponent<Rigidbody2D>().AddForce(Vector3.right * 2, ForceMode2D.Impulse);
         else
             ball.GetComponent<Rigidbody2D>().AddForce(Vector3.left * 2, ForceMode2D.Impulse);
+    }
+
+    public void FreezeBall(params GameObject[] balls) // Params
+    {
+        foreach (GameObject item in balls)
+        {
+            if (item != null)
+            {
+                currentVelocity = item.GetComponent<Rigidbody2D>().velocity;
+                item.GetComponent<Rigidbody2D>().isKinematic = true;
+                item.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            }
+        }
+    }
+
+    public void UnFreezeBall(params GameObject[] balls)//Params
+    {
+        foreach (GameObject item in balls)
+        {
+            if (item != null)
+            {
+                item.GetComponent<Rigidbody2D>().isKinematic = false;
+                item.GetComponent<Rigidbody2D>().AddForce(currentVelocity, ForceMode2D.Impulse);
+            }
+        }
     }
 }
---
+++ SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Managers/FreezeManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FreezeManager : MonoBehaviour
+{
+    public static FreezeManager fm;
+    public Text freezeTimeText;
+    public GameObject freezeTimeCount;
+    public float freezeTime;
+    public bool freeze;
+
+    void Awake()
+    {
+        if (fm == null)
+            fm = this;
+        else if (fm != this)
+            Destroy(gameObject);
+    }
+
+    void Start ()
+    {
+        freezeTimeCount.SetActive(false);
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void StartFreeze()
+    {
+        freezeTime = 3;
+
+        if (!freeze)
+            StartCoroutine(FreezeTime());
+    }
+
+    public IEnumerator FreezeTime()
+    {
+        freeze = true;
+
+        foreach (GameObject item in BallManager.bm.balls)
+        {
+            if (item != null)
+                item.GetComponent<Ball>().FreezeBall(item);
+        }
+
+        freezeTimeCount.SetActive(true);
+
+        while (freezeTime > 0)
+        {
+            freezeTime -= Time.deltaTime;
+            freezeTimeText.text = freezeTime.ToString("f2");
+            yield return null;
+        }
+
+        freezeTimeCount.SetActive(false);
+        freezeTime = 0;
+
+        foreach (GameObject item in BallManager.bm.balls)
+        {
+            if (item != null)
+                item.GetComponent<Ball>().UnFreezeBall(item);
+        }
+
+        freeze = false;
+    }
+}
