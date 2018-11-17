54ce6848330c29be6f662d2fcf48456a7fa832e6
AmayaJuan <jpva360@gmail.com>
2018-11-17 14:03:22 -0500

    Super Pang
    (Background & Border)

---
+++ SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/BackGround/BorderColor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BorderColor : MonoBehaviour
+{
+    bool danger;
+    float time;
+    Image borderColor;
+    Color normalColor;
+    Color dangerColor;
+
+    void Awake()
+    {
+        borderColor = GetComponent<Image>();
+    }
+
+    void Start ()
+    {
+        normalColor = borderColor.color;
+        dangerColor = new Color((157f / 255f), 0f, (20f / 255f));
+	}
+	
+	void Update ()
+    {
+        time = GameManager.gm.time;
+
+        if (time < 15 && !danger)
+            StartCoroutine(Danger());
+	}
+
+    IEnumerator Danger()
+    {
+        danger = true;
+
+        while (time > 0)
+        {
+            if (borderColor.color == normalColor)
+                borderColor.color = dangerColor;
+            else
+                borderColor.color = normalColor;
+
+            yield return new WaitForSeconds(.3f);
+        }
+    }
+}
