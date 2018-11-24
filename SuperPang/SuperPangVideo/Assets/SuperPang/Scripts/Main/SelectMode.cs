3bcdfde765714e064eb52d1cfdbad837707bc95f
AmayaJuan <jpva360@gmail.com>
2018-11-23 20:26:21 -0500

    ssw
    ss

--- SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Main/SelectMode.cs
+++ SuperPang/SuperPangVideo/Assets/SuperPang/Scripts/Main/SelectMode.cs
@@ -20,28 +20,28 @@ public class SelectMode : MonoBehaviour
     {
         if (tour)
         {
-            tourModeImage.color = new Color(1, 1, 1);
-            tourModeText.color = new Color(1, 1, 1);
+            //tourModeImage.color = new Color(1, 1, 1);
+            //tourModeText.color = new Color(1, 1, 1);
 
-            panicModeImage.color = new Color(1, 1, 0, .5f);
-            panicModeText.color = new Color(1, 1, 0, .5f);
+            //panicModeImage.color = new Color(1, 1, 0, .5f);
+            //panicModeText.color = new Color(1, 1, 0, .5f);
 
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-                tour = false;
+            //if (Input.GetKeyDown(KeyCode.RightArrow))
+            //    tour = false;
 
             if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0))
                 SceneManager.LoadScene("Tour_01");
         }
         else
         {
-            tourModeImage.color = new Color(1, 1, 0, .5f);
-            tourModeText.color = new Color(1, 1, 0, .5f);
+            //tourModeImage.color = new Color(1, 1, 0, .5f);
+            //tourModeText.color = new Color(1, 1, 0, .5f);
 
-            panicModeImage.color = new Color(1, 1, 1);
-            panicModeText.color = new Color(1, 1, 1);
+            //panicModeImage.color = new Color(1, 1, 1);
+            //panicModeText.color = new Color(1, 1, 1);
 
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-                tour = true;
+            //if (Input.GetKeyDown(KeyCode.LeftArrow))
+            //    tour = true;
 
             if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0))
                 SceneManager.LoadScene("Panic");
